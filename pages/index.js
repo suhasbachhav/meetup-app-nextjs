761b1b8d7a03a582a5e8bf411cb9d029880a62a7
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-30 23:24:59 +0530

    meetup add

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
-function Home() {
+import MeetupList from "@/components/meetups/MeetupList";
+
+const DUMMY_MEETUP= [{
+    id:'m1',
+    title: 'A first meetup',
+    image: 'https://media.istockphoto.com/id/480807998/photo/ramkund-at-panchavati-in-nasik-india.jpg?s=1024x1024&w=is&k=20&c=jh8TYI8JnKDtRrOtYmTZeW4AKz27aaq_7yBWZgXZjJg=',
+    address: 'test address add nashik',
+    description: 'lorem lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum'
+},{
+    id:'m2',
+    title: 'A first meetup',
+    image: 'https://media.istockphoto.com/id/480807998/photo/ramkund-at-panchavati-in-nasik-india.jpg?s=1024x1024&w=is&k=20&c=jh8TYI8JnKDtRrOtYmTZeW4AKz27aaq_7yBWZgXZjJg=',
+    address: 'test address add nashik',
+    description: 'lorem lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum'
+},{
+    id:'m3',
+    title: 'A first meetup',
+    image: 'https://media.istockphoto.com/id/480807998/photo/ramkund-at-panchavati-in-nasik-india.jpg?s=1024x1024&w=is&k=20&c=jh8TYI8JnKDtRrOtYmTZeW4AKz27aaq_7yBWZgXZjJg=',
+    address: 'test address add nashik',
+    description: 'lorem lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum'
+},]
+
+function HomePage() {
     return ( 
-        <h1>HOME</h1>
+        <MeetupList meetups={DUMMY_MEETUP}/>
      );
 }
 
-export default Home;
+export default HomePage;
