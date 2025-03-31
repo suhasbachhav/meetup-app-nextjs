88b469664add219519b785e50c4929251ce83d08
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-31 14:33:24 +0530

    mongo setup

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,23 @@
+import { MongoClient } from "mongodb";
 
 
-function handler(req, res){
+async function handler(req, res){
     if(req.method === 'POST'){
         const data = req.body;
 
-        consst (title, image, address, description ) =  data;
+    
+        const client = await MongoClient.connect('mongodb+srv://admin:admin@cluster0.jlz31gi.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0')
+        const db = client.db();
+
+        const meetupCollection = db.collection('meetups');
+        const result = await meetupCollection.insertOne(data);
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201);
+        res.json({message: 'Meetup inserted'});
     }
 }
 
