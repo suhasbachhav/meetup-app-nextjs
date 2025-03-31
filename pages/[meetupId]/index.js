f2c663e08ef1402f4dd43f7684ec6feb90f1a35e
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-31 11:09:39 +0530

    meetup details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetailsPage() {
+    return ( 
+        <MeetupDetail 
+        title='A first meetup'
+        image='https://media.istockphoto.com/id/480807998/photo/ramkund-at-panchavati-in-nasik-india.jpg?s=1024x1024&w=is&k=20&c=jh8TYI8JnKDtRrOtYmTZeW4AKz27aaq_7yBWZgXZjJg='
+        address='test address add nashik'
+        description='lorem lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum lipsum lorem lipsum'
+        />
+     );
+}
+
+export default MeetupDetailsPage;
